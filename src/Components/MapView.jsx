8dfaa7cf9f7997e4a20b38f087ea038d36b613c0
César Rosales R <cesar39299@gmail.com>
2026-04-05 03:24:49 -0500

    Version 05/04/26 03:24

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -5,6 +5,15 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet.heat";
 
+// FIX iconos
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
 // ===============================
 // 🔥 HEATMAP COMPONENT
 // ===============================
@@ -141,8 +150,8 @@ const MapView = ({ filters, onPropertiesLoaded, onLoading, selectedProperty }) =
             style={{ height: "100vh", width: "100%" }}
         >
             <TileLayer
-                attribution="&copy; OpenStreetMap"
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; OpenStreetMap & Carto'
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
             />
 
             <Heatmap data={heatmapData} />
